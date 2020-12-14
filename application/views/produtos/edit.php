463341b79b7711bbcb6d5eebebd23965e097adb2
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-12-14 20:05:06 -0300

    Criação das views Edit e index de produtos
    Criação das views Edit e index de produtos

---
+++ application/views/produtos/edit.php
@@ -0,0 +1,99 @@
+
+
+<?php $this->load->view('layout/sidebar'); ?>
+
+
+
+<!-- Main Content -->
+<div id="content">
+
+    <?php $this->load->view('layout/navbar'); ?>
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?php echo base_url('/'); ?>">Início</a></li>
+                <li class="breadcrumb-item"><a href="<?php echo base_url('servicos'); ?>">Serviços</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><?php echo $titulo; ?></li>
+            </ol>
+        </nav>
+
+        <!-- DataTales Example -->
+        <div class="card shadow ">
+            <div class="card-header py-3 pb-0 mb-0 form-control form-control-user-date">
+                <p><strong><i class="fas fa-clock" style="color: blue;"></i>&nbsp;Última Edição:  </strong><?php echo formata_data_banco_com_hora($servico->servico_data_alteracao); ?></p>
+            </div>
+
+            <div class="card-body pt-0 mt-0">
+                <form method="POST" name="form_edit">
+
+                    <!-- Dados pessoais -->
+                    <fieldset class="m-2 mt-0 border pl-2 pr-2 pb-1 pt-1">
+                        <legend class="font-small"><i class="fas fa-screwdriver" style="color: orange;"></i>&nbsp;Serviço</legend>
+                        <div class="form-group row">
+
+                            <div class="col-md-3">
+                                <label for="formGroupExampleInput">Serviço</label>
+                                <input type="text" class="form-control form-control-user-date  h-50" name="servico_nome" placeholder="Digite o nome do servico" value="<?php echo $servico->servico_nome ?>">
+                                <?php echo form_error('servico_nome', '<small class="form-text text-danger">', '</small>'); ?>
+                            </div>
+                            
+                            <div class="col-md-3">
+                                <label for="formGroupExampleInput">Descrição</label>
+                                <input type="text" class="form-control form-control-user-date  h-50" name="servico_descricao" placeholder="Descrição do Serviço" value="<?php echo $servico->servico_descricao ?>">
+                                <?php echo form_error('servico_descricao', '<small class="form-text text-danger">', '</small>'); ?>
+                            </div>
+
+                            <div class = "col-md-3">
+                                <label for = "formGroupExampleInput">Valor R$</label>
+                                <input type = "text" class = "form-control form-control-user-date h-50 money" name = "servico_preco" placeholder = "Valor R$" value="<?php echo $servico->servico_preco ?>">
+                                <?php echo form_error('servico_preco', '<small class="form-text text-danger">', '</small>');
+                                ?>
+                            </div>
+                            
+                            <div class="col-md-3">
+                                <label for="formGroupExampleInput">Situação</label>
+                                <select class="form-control form-control-user-date h-50" name="servico_ativo">
+
+                                    <option class="h-50" value="1" <?php echo($servico->servico_ativo == 1) ? 'selected' : '' ?>>Ativo</option>
+                                    <option class="h-50" value="0" <?php echo($servico->servico_ativo == 0) ? 'selected' : '' ?>>Inativo</option>
+
+                                </select>
+                            </div>
+
+                        </div>
+
+                    </fieldset>
+
+
+                    <!-- campos ocultos -->
+                    <!-- 
+                    
+                    <div class="form-group row">
+
+                        <input type="hidden" name="servico_id" value="<?php echo $servico->servico_id ?>"  />
+
+                    </div>
+                    
+                    -->
+                    <div class="form-group row float-right mt-2 mb-0">
+                        <div class="col-md-12 float-right">
+                            <a title="Voltar para a lista de servicoes" href="<?php echo base_url('servicos'); ?>" class="btn btn-success btn-sm mr-2"><i class="fas fa-arrow-left"></i>&nbsp; Voltar</i></a>
+                            <button type="submit" class="btn btn-primary btn-sm float-right">Salvar</button>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+</div>
+<!-- comment -->
+
+
+</div>
+<!-- End of Main Content -->
+
---
+++ application/views/produtos/index.php
@@ -0,0 +1,136 @@
+
+<?php $this->load->view('layout/sidebar'); ?>
+
+
+
+<!-- Main Content -->
+<div id="content">
+
+    <?php $this->load->view('layout/navbar'); ?>
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?php echo base_url('/'); ?>">Início</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><?php echo $titulo; ?></li>
+            </ol>
+        </nav>
+
+        <!-- Para erro -->
+        <?php if ($message = $this->session->flashdata('error')): ?>
+
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong><i class="fas fa-exclamation-triangle"></i>&nbsp; <?php echo $message ?></strong> 
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                </div>
+
+            </div>
+
+        <?php endif; ?>
+
+        <!-- para sucesso -->
+        <?php if ($message = $this->session->flashdata('sucesso')): ?>
+
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong><i class="far fa-smile-wink"></i>&nbsp; <?php echo $message ?></strong> 
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                </div>
+
+            </div>
+
+        <?php endif; ?>
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            
+            <div class="card-header py-3">
+                <label><h4>Produtos</h4></label>
+                <a title="Cadastrar novo Produto" href="<?php echo base_url('produtos/add') ?>" class="btn btn-success btn-sm float-right"><i class="fas fa-tags"></i>&nbsp;&nbsp;Novo</i></i></a>
+            </div>
+            <div class="card-body pt-0">
+                <div class="table-responsive">
+                    <table class="table table-bordered dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th class="text-center no-sort">ID</th>
+                                <th class="text-center">Código do Produto</th>
+                                <th class="text-center">Nome do Produto</th>
+                                <th class="text-center">Preço</th>
+                                <th class="text-center">Marca</th>
+                                <th class="text-center">Categoria</th>
+                                <th class="text-center pr-2">Estoque Mínimo</th>
+                                <th class="text-center pr-2">Qtde em Estoque</th>
+                                <th class="text-center pr-2">Situação</th>
+                                <th class="text-center no-sort">ações</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <?php foreach ($produtos as $produto): ?>
+                                <tr>
+                                    <td class="text-center"><?php echo $produto->produto_id?></td>
+                                    <td class="text-center"><?php echo $produto->produto_codigo ?></td>
+                                    <td class="text-center"><?php echo $produto->produto_descricao ?></td>
+                                    <td class="text-center money2"><?php echo $produto->produto_preco_venda ?></td>
+                                    <td class="text-center"><?php echo $produto->marca ?></td>
+                                    <td class="text-center"><?php echo $produto->categoria ?></td>
+                                    <td class="text-center"><?php echo '<span class="badge badge-success btn-sm">' .$produto->produto_estoque_minimo. '</span>' ?></td>
+                                    <td class="text-center"><?php echo ($produto->produto_estoque_minimo == $produto->produto_qtde_estoque ? '<span class="badge badge-danger btn-sm">'.$produto->produto_qtde_estoque.'</span>' : '<span class="badge badge-secondary btn-sm">'.$produto->produto_qtde_estoque.'</span>' )?></td>
+                                    <td class="text-center">
+                                        <?php echo ($produto->produto_ativo == 1 ? '<span class="badge badge-info btn-sm">Ativa</span>' : '<span class="badge badge-primary btn-sm">Inativa</span>'); ?></td>
+                                    <td class="text-center">
+                                        <a title="Editar" href="<?php echo base_url('produtos/edit/' . $produto->produto_id); ?>" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></i></a>
+                                        <a title="Excluir" href="javascript(void)" data-toggle="modal" data-target="#produto-<?php echo $produto->produto_id; ?>" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></i></a>
+                                    </td>
+                                </tr>
+
+                                 <!-- Delete Modal-->
+                            <div class="modal fade" id="produto-<?php echo $produto->produto_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Deseja realmente Excluir esse registro?</h5>
+                                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">Selecione <strong class="text-success text-uppercase">"Sim"</strong> para Deletar ou <strong class="text-danger text-uppercase">"Não"</strong> para cancelar!</div>
+                                        <div class="modal-footer">
+                                            <button class="btn btn-danger" type="button" data-dismiss="modal">Não</button>
+                                            <a class="btn btn-success" href="<?php echo base_url('categorias/del/' . $produto->produto_id); ?>">Sim</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <?php endforeach; ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>        
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
